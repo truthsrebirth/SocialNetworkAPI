90e770ddb02a3c8f3fc4d7c6a3aca6104b0a0454
Reggie P <reginaldpitts1@gmail.com>
2022-12-02 19:51:33 -0600

    edit server file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-// declare packages as variables
+// import the modules
 const express = require('express')
 const mongoose = require('mongoose');
 const moment = require('moment');
 
+// Initializing the express and port number
 const app = express()
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
