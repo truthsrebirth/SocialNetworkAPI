89637629cb6517b3babc5ae6a1c9559987361684
Reggie P <reginaldpitts1@gmail.com>
2022-12-04 08:52:39 -0600

    editing server js

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const moment = require('moment');
 const app = express()
 const PORT = process.env.PORT || 3001;
 
+// Link to the Routes folder within the directory
+app.use("/api", require("./Routes"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// This will log in the terminal
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
